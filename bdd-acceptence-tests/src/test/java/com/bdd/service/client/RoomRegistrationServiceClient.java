d530e6be68b10288d814bca2939c8b5a782336a9
jgoldsmith613 <justin.goldsmith613@gmail.com>
2015-04-30 00:46:36 -0400

    change port to something openshift allows

--- bdd-acceptence-tests/src/test/java/com/bdd/service/client/RoomRegistrationServiceClient.java
+++ bdd-acceptence-tests/src/test/java/com/bdd/service/client/RoomRegistrationServiceClient.java
@@ -14,7 +14,7 @@ import com.bdd.service.RoomRegistrationService;
 
 public class RoomRegistrationServiceClient implements RoomRegistrationService {
 
-	private static final String ROOT_URL = "http://localhost:8080/api/room";
+	private static final String ROOT_URL = "http://localhost:15151/api/room";
 
 	public boolean registerRoom(Room room) {
 		if (room == null) {
